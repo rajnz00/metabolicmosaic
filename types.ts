066fd58b3ac251f2c7446ad2d1f6270816ac9e62
rajnz00 <raj-macfarland@xtra.co.nz>
2025-09-30 13:32:42 +1300

    Fix duplicate type definitions

--- types.ts
+++ types.ts
@@ -1,10 +1,4 @@
-
 export interface BlogPost {
-  slug: string;
-  title: string;
-  author: string;
-  publishedDate: string;
-  excerpt: string;export interface BlogPost {
   slug: string;
   title: string;
   author: string;
@@ -21,19 +15,6 @@ export interface Product {
   price: number;
   imageUrl: string;
   stripeLink?: string;
-}
-
-export interface Guide extends Product {}
-  content: string;
-  imageUrl: string;
-}
-
-export interface Product {
-  id: string;
-  name: string;
-  description: string;
-  price: number;
-  imageUrl: string;
 }
 
 export interface Guide extends Product {}
